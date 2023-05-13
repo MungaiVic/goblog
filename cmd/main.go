87564742239da1a700991b51a5bc96a681662374
MungaiVic <victornmungai@gmail.com>
2023-05-13 20:35:41 +0300

    Initialize fiber

--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
 	fmt.Println("Hello bloggers!")
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Status(fiber.StatusOK).JSON("Hello there!")
+	})
+	log.Fatal(app.Listen(":3000"))
 }
