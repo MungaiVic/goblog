0a3298c661eac89ebea73fee1d3446e380032cc9
MungaiVic <victornmungai@gmail.com>
2023-05-13 20:40:16 +0300

    Implement basic logging

--- cmd/main.go
+++ cmd/main.go
@@ -5,11 +5,18 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 func main() {
 	fmt.Println("Hello bloggers!")
 	app := fiber.New()
+
+	app.Use(logger.New(logger.Config{
+		Format: "[${time}] ${status} - ${method} ${path}\n",
+		TimeZone: "Africa/Nairobi",
+		TimeFormat: "2006-01-02 15:04:05",
+	}))
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.Status(fiber.StatusOK).JSON("Hello there!")
