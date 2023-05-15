3dc0b4debb61b5190be9a5557c9f4ad649d16b2c
MungaiVic <victornmungai@gmail.com>
2023-05-15 17:53:07 +0300

    Create Blog endpoints

--- internal/handlers/blogs.go
+++ internal/handlers/blogs.go
@@ -6,10 +6,19 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-func SetupBlogRoutes(group *fiber.Group, svc service.BlogService){
+func SetupBlogRoutes(group *fiber.Group, svc service.BlogService) {
 	blogRoutes := group.Group("/blog")
 
 	blogRoutes.Get("/", func(c *fiber.Ctx) error {
 		return svc.GetAllBlogs(c)
+	})
+	blogRoutes.Post("/", func(c *fiber.Ctx) error {
+		return svc.CreateBlog(c)
+	})
+	blogRoutes.Patch("/:id", func(c *fiber.Ctx) error {
+		return svc.EditBlog(c)
+	})
+	blogRoutes.Delete("/:id", func(c *fiber.Ctx) error {
+		return svc.DeleteBlog(c)
 	})
 }
--- internal/service/blog.go
+++ internal/service/blog.go
@@ -2,18 +2,25 @@ package service
 
 import "github.com/gofiber/fiber/v2"
 
-func GetAllBlogs(c *fiber.Ctx) error {
+type blogImpl struct {
+}
+
+func NewBlogService() BlogService {
+	return &blogImpl{}
+}
+
+func (blog blogImpl) GetAllBlogs(c *fiber.Ctx) error {
 	return c.JSON("Here are all blogs")
 }
-func GetBlog(c *fiber.Ctx) error {
+func (blog blogImpl) GetBlog(c *fiber.Ctx) error {
 	return c.JSON("Here is a blog")
 }
-func CreateBlog(c *fiber.Ctx) error {
+func (blog blogImpl) CreateBlog(c *fiber.Ctx) error {
 	return c.JSON("Here you create blogs")
 }
-func EditBlog(c *fiber.Ctx) error {
+func (blog blogImpl) EditBlog(c *fiber.Ctx) error {
 	return c.JSON("Here you edit blogs")
 }
-func DeleteBlog(c *fiber.Ctx) error {
+func (blog blogImpl) DeleteBlog(c *fiber.Ctx) error {
 	return c.JSON("Here you delete blogs")
 }
